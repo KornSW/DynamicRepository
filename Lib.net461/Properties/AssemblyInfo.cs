d2128b11ba3613709bac7836fea589109269d11a
T. Korn <email@tobiaskorn.de>
2021-04-25 12:17:48 +0200

    initial commit

---
+++ Lib.net461/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KornSW.DynamicRepository")]
+[assembly: AssemblyDescription("provides functionality to supply repositories on a generic way without writing too much redundant code")]
+[assembly: AssemblyProduct("KornSW.DynamicRepository")]
+
+[assembly: AssemblyCompany("KornSW")]
+[assembly: AssemblyCopyright("Copyright © KornSW")]
+[assembly: AssemblyTrademark("KornSW")]
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("EFB002A9-1841-8C81-44AB-2F9172ABCDE4")]
+
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyInformationalVersion("1.1.0-localbuild")]
+//DONT ADD: [assembly: AssemblyFileVersion] !!!
---
+++ Tests.code/DynamicLinqExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Data {
+
+  [TestClass]
+  public class DynamicLinqExtensionsTests {
+
+    //[TestMethod]
+    //public void GetNavigationsShouldReturnOneProperty() {
+
+    //  Type t = typeof(MockEntity);
+    //  Dictionary<PropertyInfo, Type> result = t.GetNavigations(true, true, true, true);
+
+    //  Assert.AreEqual(1, result.Count);
+    //  Assert.AreEqual(nameof(MockEntity.Parent), result.Keys.First().Name);
+
+    //}
+
+  }
+
+}
